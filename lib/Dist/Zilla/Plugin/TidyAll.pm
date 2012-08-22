5debc4bd3e36eaa49f25dddf858e8e673e9216b9
Jonathan Swartz <swartz@pobox.com>
2012-08-22 02:08:31 +0000

    Import of JSWARTZ/Dist-Zilla-Plugin-TidyAll-0.03 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-TidyAll
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         JSWARTZ/Dist-Zilla-Plugin-TidyAll-0.03.tar.gz
    gitpan-cpan-author:       JSWARTZ
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/TidyAll.pm
+++ lib/Dist/Zilla/Plugin/TidyAll.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Plugin::TidyAll;
 BEGIN {
-  $Dist::Zilla::Plugin::TidyAll::VERSION = '0.02';
+  $Dist::Zilla::Plugin::TidyAll::VERSION = '0.03';
 }
 use Cwd qw(realpath);
 use Code::TidyAll;
@@ -61,7 +61,7 @@ Dist::Zilla::Plugin::TidyAll - Apply tidyall to files in Dist::Zilla
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 SYNOPSIS
 
